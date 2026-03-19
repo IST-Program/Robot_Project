4075f35106442cd2274c1c5a664ea3de1d276de6
DesmondHughes25 <desmonddhughes@gmail.com>
2026-03-19 12:58:47 -0500

    p

---
+++ def.py
@@ -0,0 +1,6 @@
+ def pickitems():
+        Hardware = 0
+        lumber = 0
+        paint = 0
+        Item_selection = input("Select Hardware, Lumber, Paint")
+        Selection_number = int(input("How many of item"))
---
+++ part2.py
@@ -0,0 +1,40 @@
+def fillBox():
+    fullbox = 0
+    items = 0
+
+    items = int(input("Enter the numbers of items to go into box. 60 max")) 
+
+    if items < 60:
+        
+        print("add test")
+    elif items == 60:
+        fullbox += 1
+        
+        print("The number is", fullbox)
+        items = 60
+        print("The number is", items)
+    else:
+        fullbox += 1
+        items -= 60
+        print("This many boxs where made", fullbox)
+        print("This is ow many items are left", items)
+        
+
+fillBox()
+
+def palllet():
+    pallets = 0
+
+    while fullbox < 1:
+    if fullbox == 6
+
+
+    def pickitems():
+        Hardware = 0
+        lumber = 0
+        paint = 0
+        Item_selection = input("Select Hardware, Lumber, Paint")
+        Selection_number = int(input("How many of item"))
+        
+
+
