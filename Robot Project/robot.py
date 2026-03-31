6e0ced5bce66e536af7c307da6d5ca41eeba0c48
ereed27 <er092@lions.eastms.edu>
2026-03-31 12:51:12 -0500

    updated 2
    yep

--- Robot Project/robot.py
+++ Robot Project/robot.py
@@ -1,44 +1,11 @@
-# DEF
-def pickitems():
-       Hardware = 0
-       lumber = 0
-       paint = 0
-       Item_selection = input("Select Hardware, Lumber, Paint")
-       Selection_number = int(input("How many of item"))
-
-
 # CHP 1
 name = "barry"
 charge = int(100)
 print("charge:", charge)
-
+fullbox = 0
+items = 0
 def fillBox():
-    fullbox = 0
-    items = 0
-
-    items = int(input("Enter the numbers of items to go into box. 60 max: ")) 
-
-    if items < 60:
-        
-        print("add test")
-    elif items == 60:
-        fullbox += 1
-        
-        print("The number is", fullbox)
-        items = 60
-        print("The number is", items)
-    else:
-        fullbox += 1
-        items -= 60
-        print("This many boxs where made", fullbox)
-        print("This is ow many items are left", items)
-fillBox()
-
-
-# CHP 2
-def fillBox():
-    fullbox = 0
-    items = 0
+    
 
     items = int(input("Enter the numbers of items to go into box. 60 max")) 
 
